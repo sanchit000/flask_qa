f9a27eccc60c319cbc67a10cd6bdbb28ffa8472c
sanchit000 <42431580+sanchit000@users.noreply.github.com>
2022-11-03 21:53:11 +0530

    Update DB
    Update DB

--- flask_qa/settings.py
+++ flask_qa/settings.py
@@ -4,6 +4,6 @@ import os
 """
 Setup the DB variable from env file
 """
-SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
