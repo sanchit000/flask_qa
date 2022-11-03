98059817e12cb00cb77f2a1435b2ef63f67a9d0c
sanchit000 <42431580+sanchit000@users.noreply.github.com>
2022-11-03 22:37:17 +0530

    Added procfile

--- flask_qa/settings.py
+++ flask_qa/settings.py
@@ -4,6 +4,6 @@ import os
 """
 Setup the DB variable from env file
 """
-SQLALCHEMY_DATABASE_URI = 'postgres://rwcfgoyqzjbckz:c65fba435b820fc5037fd3af33ce76cb5ac53a7aa1e8075ed1f3a8567875f8ee@ec2-18-215-41-121.compute-1.amazonaws.com:5432/d9717nnm2u0c14'
+SQLALCHEMY_DATABASE_URI = 'postgresql://rwcfgoyqzjbckz:c65fba435b820fc5037fd3af33ce76cb5ac53a7aa1e8075ed1f3a8567875f8ee@ec2-18-215-41-121.compute-1.amazonaws.com:5432/d9717nnm2u0c14'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
